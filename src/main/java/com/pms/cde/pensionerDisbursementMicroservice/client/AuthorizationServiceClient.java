c6e764f074e85410cb3543fadbbc94ba92f54674
Prashant Singh <52065013+PrashantTariyal@users.noreply.github.com>
2022-08-19 12:07:25 +0530

    Update AuthorizationServiceClient.java

--- src/main/java/com/pms/cde/pensionerDisbursementMicroservice/client/AuthorizationServiceClient.java
+++ src/main/java/com/pms/cde/pensionerDisbursementMicroservice/client/AuthorizationServiceClient.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
 
 
-@FeignClient(name="auth-service",url = "http://localhost:7001/auth")
+@FeignClient(name="auth-service",url = "http://65.1.111.103:7001/auth")
 public interface AuthorizationServiceClient {
 	
 	@PostMapping(value = "/authorize")
